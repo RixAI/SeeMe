5ec959b47f14678cd02fc68fc5389c11490e81be
RixAI <RixAI@outlook.com>
2025-03-16 16:31:58 +0530

    Update predictor.py

--- roop/predictor.py
+++ roop/predictor.py
@@ -1,14 +1,7 @@
 # roop/predictor.py
-# Clean dev version with NSFW logic removed.
 
 def predict_image(target_path: str) -> bool:
     return False
 
-def predict_frame(target_frame) -> bool:
-    return False
-
 def predict_video(target_path: str) -> bool:
     return False
-
-def clear_predictor() -> None:
-    pass
